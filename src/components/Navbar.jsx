93de8dfe55ec2a9a42ec2eb6628333258313becf
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-10-10 16:18:21 -0500

    trying to update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,59 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 export default function Navbar({ currentUser, handleLogout }) {
-	 const loggedIn = (
-		<>
-			{/* if the user is logged in... */}
-			<Link to="/">
-				<span onClick={handleLogout}>logout</span>
-			</Link>
+	const loggedIn = (
+		<div className='w-full h-[90px] bg-black'>
+			<div className=' max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full'>
+				<div>
+					<h1 className='text-[#00d8ff]'>Memories</h1>
+				</div>
+				<div className='hidden md:flex'>
+					<ul className='flex text-white items-center'>
+						{/* if the user is logged in... */}
+						<Link to="/">
+							<span onClick={handleLogout}>Logout</span>
+						</Link>
 
-			<Link to="/profile">
-				profile
-			</Link>
+						<Link to="/profile">
+							Profile
+						</Link>
 
-			<Link to="/newMemories">
-				<p>NewMemories</p>
-			</Link>
-      		<Link to="/memory">
-				<p>Memory</p>
-			</Link>
-      		<Link to="/memories">
-				<p>Memories</p>
-			</Link>
-      		<Link to="/editMemories">
-				<p>EditMemories</p>
-			</Link>
-		</>
-	 )
+						<Link to="/newMemories">
+							<p>New Memories</p>
+						</Link>
 
-	 const loggedOut = (
-		<>
-			{/* if the user is not logged in... */}
-			<Link to="/register">
-				register
-			</Link>
+						<Link to="/memories">
+							<p>Memories</p>
+						</Link>
 
-			<Link to="/login">
-				login
-			</Link>
-		</>
-	 )
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
+	const loggedOut = (
+		<div className='w-full h-[90px] bg-black'>
+			<div className=' max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full'>
+				<div>
+					<h1 className='text-[#00d8ff]'>DEFI</h1>
+				</div>
+				<div className='hidden md:flex'>
+					<ul className='flex text-white items-center'>
+						{/* if the user is not logged in... */}
+						<Link to="/register">
+							register
+						</Link>
 
+						<Link to="/login">
+							login
+						</Link>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 	return (
 		<nav>
 			{/* user always sees this section */}
