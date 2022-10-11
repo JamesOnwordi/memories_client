a8f2fe50d31e2b965e29215ed006c2d06a4edeef
average-alien <allentang113@gmail.com>
2022-10-11 11:10:03 -0700

    finishes edit memory form

--- src/components/pages/EditMemories.jsx
+++ src/components/pages/EditMemories.jsx
@@ -1,64 +1,83 @@
 import axios from "axios"
-import { useEffect } from "react"
-import { useState } from "react"
-import { useParams } from "react-router-dom"
+import { useState, useRef } from "react"
+import { useParams, useNavigate } from "react-router-dom"
 
 export default function EditMemories(){
-    const {id} = useParams()
-    const [data,setData] = useState({})
-    console.log(id)
-    useEffect(()=>{
-        // pull token from local storage
-     const token = localStorage.getItem('jwt')
-     // set request headers
-     const options = {
-     headers: {
-         "Authorization": token
-     }
-     }
-     console.log(id)
-        const url = process.env.REACT_APP_SERVER_URL+ '/api-v1/memories/' + id 
-        axios.get(url,options)
-        .then((response)=>{
-            console.log(response)
-            ({
-                title:'',
-                note: ''
-            })
-            console.log(console.log)
-        })
-    })
+    const [formImg, setFormImg] = useState([])
+    const [formTitle, setFormTitle] = useState('')
+    const [formNote, setFormNote] = useState('')
+    const [msg, setMsg] = useState('')
+    
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const inputRef = useRef(null)
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = async e =>{
         e.preventDefault()
-        console.log(data)
-
+        try {
+            // server url
+            const url = process.env.REACT_APP_SERVER_URL + '/api-v1/memories/' + id
+            // build form data
+            const formData = new FormData()
+            for (const file of formImg) {
+                formData.append('images', file)
+            }
+            formData.append('title', formTitle)
+            formData.append('note', formNote)
+            // pull token from local storage
+            const token = localStorage.getItem('jwt')
+            // set request headers
+            const options = {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": token
+                }
+            }
+            const response = await axios.put(url, formData, options)
+            console.log(response.data)
+            
+        } catch (error) {
+            console.log(error)
+            if (error.response) {
+                setMsg(error.response.data.msg)
+            }
+        }
     }
     return(
-        <div>
-               
-            <form  onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='image-upload'>Add more photos: </label>
+                <input 
+                    type='file'
+                    id='image-upload'
+                    accept='images/*'
+                    multiple
+                    ref={inputRef}
+                    onChange={e => setFormImg(e.target.files)}
+                />
+            </div>
 
-                 <div>
-                    <input type='file' />
-                    <p/>
-                </div>
-                <p></p>
-                <div>
-                    <p> Title: <input type="text" onChange={e=>data.title=e.target.value}/></p> 
-                    
-                </div>
-                
+            <div>
+                <label htmlFor='title'>Edit title: </label>
+                <input
+                    type='text'
+                    id='title'
+                    onChange={e => setFormTitle(e.target.value)}
+                    value={formTitle}
+                />
+            </div>
 
-                <div>
-                    <input type='text' name='note' style={{width:'500px' , height:'300px'}} onChange={e=>data.note = e.target.value}/>
-                </div>
-                <div>
-                    <p/>
-                    <button type="submit" ><p>Memorize</p></button>
-                </div>
-            </form>
-        </div>
+            <div>
+                <label htmlFor='note'>Edit note: </label>
+                <textarea
+                id='note'
+                onChange={e => setFormNote(e.target.value)}
+                value={formNote}
+                />
+            </div>
+
+            <button type="submit">Memorize</button>
+        </form>
     )
 
 }
